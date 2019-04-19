505bb1e07de4346e0e776bedc409a59030b23970
Ankesh Bharti <ankesh.b@idsil.com>
2019-04-19 16:42:07 +0530

    worked on JWT token authentication

--- Awemedia.Admin.AzureFunctions/Awemedia.Admin.AzureFunctions/Extensions/AuthorizationExtensions.cs
+++ Awemedia.Admin.AzureFunctions/Awemedia.Admin.AzureFunctions/Extensions/AuthorizationExtensions.cs
@@ -1,8 +1,12 @@
-﻿using Awemedia.Chargestation.Api.Helpers;
+﻿using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net;
 using System.Net.Http;
-using System.Text;
+using System.Security.Claims;
 
 namespace Awemedia.Chargestation.AzureFunctions.Helpers
 {
@@ -10,7 +14,34 @@ namespace Awemedia.Chargestation.AzureFunctions.Helpers
     {
         public static bool IsAuthorized(this HttpRequestMessage req)
         {
-            return true;
+            try
+            {
+                if (!string.IsNullOrEmpty(Convert.ToString(req.Headers.Authorization)))
+                {
+                    string jwtToken = req.Headers.Authorization.Parameter;
+                    TokenValidationParameters validationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        ValidateIssuer = true,
+                        ValidateLifetime = true,
+                        ValidateAudience = true,
+                        ValidAudience = Environment.GetEnvironmentVariable("ValidAudience"),
+                        ValidIssuer = Environment.GetEnvironmentVariable("ValidIssuer"),
+                        IssuerSigningKeyResolver = (s, securityToken, identifier, parameters) =>
+                        {
+                            var json = new WebClient().DownloadString(parameters.ValidIssuer + "/.well-known/jwks.json");
+                            var keys = JsonConvert.DeserializeObject<JsonWebKeySet>(json).Keys;
+                            return (IEnumerable<SecurityKey>)keys;
+                        }
+                    };
+                    ClaimsPrincipal principal = new JwtSecurityTokenHandler().ValidateToken(jwtToken, validationParameters, out SecurityToken validatedToken);
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
     }
-}
+} 
