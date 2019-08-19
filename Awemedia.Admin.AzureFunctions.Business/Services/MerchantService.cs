b5236082c34180a46fd669acb2e7bb6fceb4d7e7
Ankesh Bharti <ankesh.b@idsil.com>
2019-08-19 12:28:37 +0530

    worked on merchant search by name

--- Awemedia.Admin.AzureFunctions.Business/Services/MerchantService.cs
+++ Awemedia.Admin.AzureFunctions.Business/Services/MerchantService.cs
@@ -123,5 +123,9 @@ namespace Awemedia.Admin.AzureFunctions.Business.Services
         {
             return MappingProfile.MapMerchantModelObject(_baseService.GetById(id, navigationalProperties, includedProperties));
         }
+        public List<object> Search(string keyword)
+        {
+            return _baseService.Where(m => m.BusinessName.ToLower().Contains(keyword)).Select(s => new { s.Id, s.BusinessName }).ToList<object>();
+        }
     }
 }
