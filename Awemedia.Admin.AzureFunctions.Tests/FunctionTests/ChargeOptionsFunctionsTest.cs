0131d75c49f3f5d06d654b49d09f528a79b1983e
Baljeet Singh <baljeet.upwrk@awepay.com>
2023-03-03 15:39:40 +0530

    fixed all existing unit test cases

--- Awemedia.Admin.AzureFunctions.Tests/FunctionTests/ChargeOptionsFunctionsTest.cs
+++ Awemedia.Admin.AzureFunctions.Tests/FunctionTests/ChargeOptionsFunctionsTest.cs
@@ -1,5 +1,6 @@
 ﻿using Awemedia.Admin.AzureFunctions.Business.Infrastructure.ErrorHandler;
 using Awemedia.Admin.AzureFunctions.Business.Interfaces;
+using Awemedia.Admin.AzureFunctions.Business.Models;
 using Awemedia.Admin.AzureFunctions.Business.Repositories;
 using Awemedia.Admin.AzureFunctions.Business.Services;
 using Awemedia.Admin.AzureFunctions.DAL.DataContracts;
@@ -7,12 +8,15 @@ using Awemedia.Admin.AzureFunctions.Functions;
 using Awemedia.chargestation.API.tests.Common;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using OidcApiAuthorization;
+using Moq;
 using OidcApiAuthorization.Abstractions;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Claims;
 using System.Text;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Awemedia.Admin.AzureFunctions.Tests.FunctionTests
@@ -21,7 +25,7 @@ namespace Awemedia.Admin.AzureFunctions.Tests.FunctionTests
     {
         private const string Name = "AwemediaConnection";
         private readonly ChargeOptionFunctions chargeOptionFunctions;
-        private readonly IChargeOptionService chargeOptionsService;
+        private readonly Mock<IChargeOptionService> chargeOptionsService;
         private readonly IBaseService<ChargeOptions> _baseService;
         private readonly IBaseRepository<ChargeOptions> _repository;
         private readonly IErrorHandler _errorHandler;
@@ -36,21 +40,25 @@ namespace Awemedia.Admin.AzureFunctions.Tests.FunctionTests
                 .UseSqlServer(connectionString)
                 .Options;
         }
-        public ChargeOptionsFunctionsTest(IApiAuthorization apiAuthorization)
+
+        public ChargeOptionsFunctionsTest()
         {
             var context = new AwemediaContext(dbContextOptions);
             _errorHandler = new ErrorHandler();
             _repository = new BaseRepository<ChargeOptions>(context, _errorHandler);
             _baseService = new BaseService<ChargeOptions>(_repository);
-            chargeOptionsService = new ChargeOptionsService(_baseService);
-            chargeOptionFunctions = new ChargeOptionFunctions(apiAuthorization);
+            chargeOptionsService = new Mock<IChargeOptionService>();
+            // chargeOptionFunctions = new ChargeOptionFunctions(apiAuthorization);
         }
+
         [Fact]
         public void Get_WhenCalled_ReturnsAllItems()
         {
             HttpRequestMessage httpRequestMessage = Common.CreateRequest();
+            httpRequestMessage.RequestUri = new Uri("http://localhost/test?Search=test&IsActive=true");//BaseSearchFilter model class
             httpRequestMessage.Headers.CacheControl.MaxAge = new TimeSpan(0, 2, 0);
-            var okResult = chargeOptionFunctions.Get(httpRequestMessage, chargeOptionsService, _errorHandler);
+            var _chargeOptionFunctions = SetAuth(true);
+            var okResult = _chargeOptionFunctions.Get(httpRequestMessage, chargeOptionsService.Object, _errorHandler);
             Assert.NotNull(okResult);
             Assert.Equal("OK", okResult.StatusCode.ToString());
         }
@@ -59,29 +67,96 @@ namespace Awemedia.Admin.AzureFunctions.Tests.FunctionTests
         public void Post_WhenCalled_InsertNewChargeOption()
         {
             HttpRequestMessage httpRequestMessage = Common.CreateRequest();
-            httpRequestMessage.Content = new StringContent("{\"Price\":\"11.00\",\"Currency\":\"RM\",\"ChargeDuration\":\"150 Mins\"}", Encoding.UTF8, "application/json");
-            var okResult = chargeOptionFunctions.Post(httpRequestMessage, chargeOptionsService, _errorHandler);
+            var content = GetChargeOptionModel(true);
+            httpRequestMessage.Content = content;
+            var _chargeOptionFunctions = SetAuth(true);
+            var okResult = _chargeOptionFunctions.Post(httpRequestMessage, chargeOptionsService.Object, _errorHandler);
             Assert.NotNull(okResult);
             Assert.Equal("OK", okResult.StatusCode.ToString());
         }
+
         [Fact]
         public void Delete_WhenCalled_MarkActiveInActiveChargeOption()
         {
             HttpRequestMessage httpRequestMessage = Common.CreateRequest();
             httpRequestMessage.Content = new StringContent("[{\"Id\":\"4\",\"IsActive\":\"true\"},{\"Id\":\"6\",\"IsActive\":\"false\"}]", Encoding.UTF8, "application/json");
-            var okResult = chargeOptionFunctions.Put(httpRequestMessage, chargeOptionsService, _errorHandler);
+            var _chargeOptionFunctions = SetAuth(true);
+            var okResult = _chargeOptionFunctions.Put(httpRequestMessage, chargeOptionsService.Object, _errorHandler);
             Assert.NotNull(okResult);
             Assert.Equal("OK", okResult.StatusCode.ToString());
+        }
+
+        private bool AddDelegate(ChargeOption chargeOption, out bool isDuplicateRecord, int id)
+        {
+            isDuplicateRecord = true;
+            return false;
         }
 
         [Fact]
         public void IsDuplicateRecord()
         {
             HttpRequestMessage httpRequestMessage = Common.CreateRequest();
-            httpRequestMessage.Content = new StringContent("{\"Price\":\"11.00\",\"Currency\":\"RM\",\"ChargeDuration\":\"150 Mins\"}", Encoding.UTF8, "application/json");
-            var okResult = chargeOptionFunctions.Post(httpRequestMessage, chargeOptionsService, _errorHandler);
+            
+            var content = GetChargeOptionModel(true);
+            httpRequestMessage.Content = content;
+
+            bool duplicate = false;
+            chargeOptionsService
+                .Setup(c => c.Add(It.IsAny<Business.Models.ChargeOption>(), out duplicate, It.IsAny<int>()))
+                .Callback(() => AddDelegate(null, out duplicate, 1))
+                .Returns(true);
+            
+            var _chargeOptionFunctions = SetAuth(true);
+            var okResult = _chargeOptionFunctions.Post(httpRequestMessage, chargeOptionsService.Object, _errorHandler);
             Assert.NotNull(okResult);
-            Assert.Equal("Conflict", okResult.StatusCode.ToString());
+            Assert.True(duplicate);
+        }
+
+        private static ChargeOptionFunctions SetAuth(bool success = true, string claimValue = "owner")
+        {
+            var auth = new Mock<IApiAuthorization>();
+            if (success)
+            {
+                var ci = new ClaimsIdentity();
+                ci.AddClaim(new Claim("extension_UserRoles", claimValue));
+                ci.AddClaim(new Claim("emails", "baljeet@awepay.com"));
+                var claims = new ClaimsPrincipal(ci);
+
+                auth
+                    .Setup(s => s.AuthorizeAsync(It.IsAny<HttpRequestHeaders>()))
+                    .Returns(Task.FromResult(new OidcApiAuthorization.Models.ApiAuthorizationResult(claims)));
+                return new ChargeOptionFunctions(auth.Object);
+            }
+            auth
+                .Setup(s => s.AuthorizeAsync(It.IsAny<HttpRequestHeaders>()))
+                .Returns(Task.FromResult(new OidcApiAuthorization.Models.ApiAuthorizationResult("BadLogin")));
+            return new ChargeOptionFunctions(auth.Object);
+        }
+
+        private StringContent GetChargeOptionModel(bool isValid)
+        {
+            Business.Models.ChargeOption option;
+            if (isValid)
+            {
+                option = new Business.Models.ChargeOption()
+                {
+                    Id = 4,
+                    IsActive = true,
+                    Currency = "RM",
+                    ChargeDuration = 150,
+                    CountryId = 75,
+                    CountryName = "Malaysia",
+                    CreatedDate = DateTime.Now,
+                    ModifiedDate = DateTime.Now,
+                    Price = 500
+                };
+            }
+            else
+            {
+                option = new Business.Models.ChargeOption() { };
+            }
+            var content = Newtonsoft.Json.JsonConvert.SerializeObject(option);
+            return new StringContent(content, Encoding.UTF8, "application/json");
         }
     }
 }
